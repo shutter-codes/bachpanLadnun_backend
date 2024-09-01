aa9c9c5eadb0b19aeed9d30b3523527d17543fa0
Naitik Sharma <devnaitik104@gmail.com>
2024-09-02 01:41:37 +0530

    server updated
    Signed-off-by: Naitik Sharma <devnaitik104@gmail.com>

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const helmet = require('helmet');
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 
